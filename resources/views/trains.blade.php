6d1c3d1838f0ffa0ccc82bb49ee749c5bb563195
Tancredi Carluccio <tanc_@hotmail.it>
2022-05-27 00:38:51 +0200

    Stampa a schermo primi dati

--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -4,16 +4,16 @@
 
 @section('content')
 
-        {{-- @forelse ($movies as $key => $movie)
+        @forelse ($trains as $key => $train)
             
-                        <h3 >{{ $movie->title }}</h3>
-                        <p >{{ $movie->original_title }}</p>
-                        <p >{{ $movie->nationality }}</p>
-                        <p >{{ $movie->date }}</p>     
-                        <p >{{ $movie->vote }}</p>                   
+                        <h3 >{{ $train->azienda }}</h3>
+                        <p >{{ $train->stazione_di_partenza }}</p>
+                        <p >{{ $train->orario_di_arrivo }}</p>
+                        <p >{{ $train->numero_carrozze }}</p>     
+                        <p >{{ $train->cancellato }}</p>                   
              
         @empty
             <p>Array vuoto</p>
-        @endforelse --}}
+        @endforelse
 
 @endsection
